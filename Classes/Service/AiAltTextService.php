7530a3a3979a793274d8d2e359ba5d3cd33ac2d8
Michał Cygankiewicz <m.cygankiewicz@amtsolution.pl>
2024-03-14 11:01:41 +0100

    Bug: alt-text generation fails for big images

--- Classes/Service/AiAltTextService.php
+++ Classes/Service/AiAltTextService.php
@@ -25,6 +25,11 @@ class AiAltTextService
 {
     public AltTextClient $altTextClient;
     public FileService $fileService;
+    private int $skippedAltTextForFiles = 0;
+    private int $failedProcessedImages = 0;
+    private int $generatedAltTexts = 0;
+    private int $fileIsNotImage = 0;
+    private int $hasAltText = 0;
 
     public function __construct(AltTextClient $altTextClient, FileService $fileService)
     {
@@ -41,7 +46,7 @@ class AiAltTextService
             $altText = $this->altTextClient->getByAssetId($file->getOriginalResource()->getUid(), $languageIsoCode);
         } catch (\Exception $e) {
             if (404 != $e->getCode()) {
-                throw new \Exception($e->getMessage());
+                throw $e;
             }
 
             return $this->altTextClient->getAltTextForFile($file, $languageIsoCode);
@@ -56,14 +61,6 @@ class AiAltTextService
     public function getListOfFiles(string $folderName): array
     {
         return $this->fileService->getFilesFromExistingFolder($folderName);
-    }
-
-    /**
-     * @return array<int|string, FileAltTextDTO>
-     */
-    public function getAltTextsOfImagesFromFolder(string $folderName): array
-    {
-        return $this->fileService->getAltTextFromMetadataOfFiles($folderName);
     }
 
     /**
@@ -87,22 +84,34 @@ class AiAltTextService
         $finalFilesWithAltText = [];
         $files = $this->getListOfFiles($folderName);
         $emptyAltTextFiles = $this->getEmptyAltTextFiles($folderName);
-        $supportedFormatsImages = ['jpg', 'png', 'gif', 'webp', 'bmp'];
 
-        try {
-            foreach ($files as $file) {
-                $fileUid = $file->getUid();
-                $fileById = $this->getFileById((string) $fileUid);
-
-                if (!isset($emptyAltTextFiles[$fileUid]) || null == $fileById || !in_array($file->getExtension(), $supportedFormatsImages)) {
-                    continue;
-                }
+        foreach ($files as $file) {
+            $fileUid = $file->getUid();
+            $fileById = $this->getFileById((string) $fileUid);
+            if (null == $fileById) {
+                continue;
+            }
+            if (!empty($fileById->getOriginalResource()->getProperty('alternative'))) {
+                $this->incrementHasAltText();
+                continue;
+            }
+            if (!$this->isSupportedImage($file->getExtension())) {
+                $this->incrementFileIsNotImage();
+                continue;
+            }
+            if (!isset($emptyAltTextFiles[$fileUid])) {
+                $this->incrementSkippedAltTextFiles();
+                continue;
+            }
+            try {
                 $emptyAltTextFiles[$fileUid]->setAltText($this->getAltText($fileById));
                 $emptyAltTextFiles[$fileUid]->setFile($file);
                 $finalFilesWithAltText[$fileUid] = $emptyAltTextFiles[$fileUid];
+                $this->incrementGeneratedAltText();
+            } catch (\Exception $e) {
+                $this->incrementFailedProcessedImages();
+                continue;
             }
-        } catch (\Exception $e) {
-            throw new \Exception($e->getMessage());
         }
 
         return $finalFilesWithAltText;
@@ -121,5 +130,57 @@ class AiAltTextService
             $metadata->offsetSet('alternative', $fileAltTextDTO->getAltText());
             $metadata->save();
         }
+    }
+
+    public function isSupportedImage(string $fileExtension): bool
+    {
+        $supportedFormatsImages = ['jpg', 'png', 'gif', 'webp', 'bmp'];
+
+        return in_array($fileExtension, $supportedFormatsImages);
+    }
+
+    public function getFailedProcessedImages(): int
+    {
+        return $this->failedProcessedImages;
+    }
+
+    public function getGeneratedAltTexts(): int
+    {
+        return $this->generatedAltTexts;
+    }
+
+    public function getFileIsNotImage(): int
+    {
+        return $this->fileIsNotImage;
+    }
+
+    public function getHasAltText(): int
+    {
+        return $this->hasAltText;
+    }
+
+    public function incrementSkippedAltTextFiles(): void
+    {
+        ++$this->skippedAltTextForFiles;
+    }
+
+    public function incrementGeneratedAltText(): void
+    {
+        ++$this->generatedAltTexts;
+    }
+
+    public function incrementFailedProcessedImages(): void
+    {
+        ++$this->failedProcessedImages;
+    }
+
+    public function incrementFileIsNotImage(): void
+    {
+        ++$this->fileIsNotImage;
+    }
+
+    public function incrementHasAltText(): void
+    {
+        ++$this->hasAltText;
     }
 }
